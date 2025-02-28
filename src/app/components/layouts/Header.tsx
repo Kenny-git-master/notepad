0fd8d9a4e905e05a99d010027025b245062adbca
unknown <m.sanusvir@gmail.com>
2025-02-28 11:51:43 +0900

    created Header and import ChakraUI

---
+++ src/app/components/layouts/Header.tsx
@@ -0,0 +1,27 @@
+// import React from "react";
+import { SITE_TITLE } from "@/app/constants/siteConfig";
+import { COLORS } from "../../styles/theme";
+import Navigation from "@/app/components/elements/Navigation";
+import { Heading, Flex } from "@chakra-ui/react";
+import { css } from "@emotion/react";
+
+const header = css({
+  height: "50",
+  backgroundColor: COLORS.PRIMARY,
+  color: COLORS.WHITE,
+  padding: "5px 20px",
+});
+
+export default function Header() {
+  return (
+    <header css={header}>
+      <Flex justify="space-between" bg="brand.100">
+        <Heading size="3xl">
+          <a href="">{SITE_TITLE}</a>
+        </Heading>
+
+        <Navigation />
+      </Flex>
+    </header>
+  );
+}
